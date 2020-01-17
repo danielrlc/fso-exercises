66733603148649c996ec930c49840972010a2d96
Daniel <daniel@MacBook-Pro.local>
2020-01-17 09:59:14 +0200

    courseinfo step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'
 
 const Header = ({ course }) => <h1>{course}</h1>
 
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+)
+
 const Content = ({
   part1,
   exercises1,
@@ -12,15 +18,9 @@ const Content = ({
   exercises3,
 }) => (
   <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
   </>
 )
 
